d08884f9a9da9723e2abe2dc4ab9bf55970fcc03
Anton Krotenok <80707699+theonlyanton@users.noreply.github.com>
2022-06-06 23:33:04 -0400

    Add files via upload

---
+++ Modal View/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+//console.log(btnsOpenModal);
+
+
+
+
+const openModal = function () {
+    //console.log('Button Clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+for(let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+
+
+
+const closedModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', closedModal);
+overlay.addEventListener('click', closedModal);
+
+document.addEventListener('keydown', function(e) {
+    //console.log("A key was pressed.");
+    //console.log(e);
+    //console.log(e.key);
+
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closedModal();
+    }
+});
